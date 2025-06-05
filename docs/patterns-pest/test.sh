ede1c62184c5516f8182b342b8871208405b3aa8
TheVeryDarkness <3266343194@qq.com>
2025-06-05 16:17:16 +0800

    Mark CVE-2020-35901-2.rpl as ready

--- docs/patterns-pest/test.sh
+++ docs/patterns-pest/test.sh
@@ -6,7 +6,8 @@ set -eux
 # docs/patterns-pest/CVE-2019-15548.rpl
 # docs/patterns-pest/CVE-2020-25016.rpl
 # docs/patterns-pest/CVE-2021-27376.rpl
-# docs/patterns-pest/CVE-2020-35898-9-MIR.rpl
+# docs/patterns-pest/CVE-2020-35898-9.rpl
+# docs/patterns-pest/CVE-2020-35901-2.rpl
 # docs/patterns-pest/cast-size-different-sizes.rpl
 # docs/patterns-pest/unsound-collection-transmute.rpl
 
@@ -16,5 +17,6 @@ RPL_PATS="docs/patterns-pest/" cargo uitest -- \
     "tests/ui/cve_2020_25016" \
     "tests/ui/cve_2021_27376" \
     "tests/ui/cve_2020_35898_9" \
+    "tests/ui/cve_2020_35901_2" \
     "tests/ui/cast_size_different_sizes" \
     "tests/ui/unsound_collection_transmute"
